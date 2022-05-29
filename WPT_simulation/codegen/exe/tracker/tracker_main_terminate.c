2dd1887b00657f002b9f3ecf70d80226fff6e075
SLEIPNOR <68347839+SLEIPNOR@users.noreply.github.com>
2022-05-29 19:36:36 +0100

    Add files via upload

---
+++ WPT_simulation/codegen/exe/tracker/tracker_main_terminate.c
@@ -0,0 +1,15 @@
+/* Include Files */
+#include "tracker_terminate.h"
+
+/* Function Definition */
+void main_terminate(void)
+{
+    /* Call the entry-point function. */
+    tracker_terminate();
+}
+
+/*
+ * File trailer for main_terminate
+ *
+ * [EOF]
+ */
